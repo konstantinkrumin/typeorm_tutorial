02c91a9ac5c71956f4c27fcaa496a4dd7621b649
Konstantin Krumin <konstantin.krumin@gmail.com>
2022-06-12 17:56:18 -0400

    first commit

---
+++ src/entity/Post.ts
@@ -0,0 +1,15 @@
+import { Entity, Column, ManyToOne } from 'typeorm';
+import Model from './Model';
+import { User } from './User';
+
+@Entity({ name: 'posts' })
+export class Post extends Model {
+	@Column()
+	title: string;
+
+	@Column()
+	body: string;
+
+	@ManyToOne(() => User)
+	user: User;
+}
---
+++ src/entity/User.ts
@@ -0,0 +1,18 @@
+import { Entity, Column, OneToMany } from 'typeorm';
+import Model from './Model';
+import { Post } from './Post';
+
+@Entity({ name: 'users' })
+export class User extends Model {
+	@Column()
+	name: string;
+
+	@Column()
+	email: string;
+
+	@Column()
+	role: string;
+
+	@OneToMany(() => Post, post => post.user)
+	posts: Post[];
+}
